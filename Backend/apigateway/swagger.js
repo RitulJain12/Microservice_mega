4d8218cea5abd0faec3b82c8723776d55a9da5d5
Ritul Jain <rituljain202@gmail.com>
2026-06-24 13:52:41 +0530

    name error fix

--- Backend/apigateway/swagger.js
+++ Backend/apigateway/swagger.js
@@ -30,7 +30,7 @@ const options = {
       },
     ],
   },
-  apis: [path.join(__dirname, './docs/*.yaml')], 
+  apis: [path.join(__dirname, './docs/*.yaml').replace(/\\/g, '/')],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
